4c360e0d9a58ea8b339c2d82ac7c7d23c6a043c4
Shawn Morford <DarkMorford@users.noreply.github.com>
2015-07-06 11:34:42 -0700

    Export assembly code from IDA project. This is syntactically correct, but missing all the variable definitions.

--- main-68k/build.bat
+++ main-68k/build.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET ASM=..\bin\asm68k.exe
-SET ASMOPTS=/j "include\*" /j "incbin\*" /p /o c+,ws+
+SET ASMOPTS=/j "include\*" /j "incbin\*" /p /o c+,ws+,ow+
 SET INFILE=bios_main.asm
 SET OUTFILE=bios_main_build
 
