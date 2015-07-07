8ec7bef0eeb2be9dadb7ab779e6eca36c094039b
DarkMorford <DarkMorford@users.noreply.github.com>
2015-07-07 14:40:56 -0700

    Add some macros. Extract (probably) the CD boot block.

--- sub-68k/include/macros.asm
+++ sub-68k/include/macros.asm
@@ -1,3 +1,12 @@
 ;   ======================================================================
 ;       Macros
 ;   ======================================================================
+
+;   Interrupt masking
+m_disableInterrupts:    macro
+	move #$2700, sr
+	endm
+
+m_enableInterrupts: macro
+	move #$2000, sr
+	endm
