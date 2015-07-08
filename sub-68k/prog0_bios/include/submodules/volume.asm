63e71d566eea6018ba6754f8bdf9d724cbb2a0f5
DarkMorford <DarkMorford@users.noreply.github.com>
2015-07-08 12:08:07 -0700

    Clean up spacing on volume functions.

--- sub-68k/prog0_bios/include/submodules/volume.asm
+++ sub-68k/prog0_bios/include/submodules/volume.asm
@@ -7,7 +7,7 @@
 
 initVolume:             ; CODE XREF: BIOS:00000356p
 	; Clear memory from $5AB6-$5AC7
-	lea volumeBitfield(a5),a0
+	lea volumeBitfield(a5), a0
 	moveq #0, d0
 	moveq #3, d1
 	@clearMemory:
@@ -29,37 +29,37 @@ initVolume:             ; CODE XREF: BIOS:00000356p
 
 _fdrset:                ; CODE XREF: sub_2946+22j initCdd+2Cp
 	; Clear volume update flag
-	bclr    #7,volumeBitfield(a5)
+	bclr #7, volumeBitfield(a5)
 
 	; Check if parameter is master/system volume
-	bclr    #$F,d1
-	beq.s   @loc_277C
+	bclr #$F, d1
+	beq.s @loc_277C
 
 	; Update master volume
-	move.w  masterVolume(a5),d0
-	move.w  d1,masterVolume(a5)
-	move.w  systemVolume(a5),d1
-	lsr.w   #4,d1
-	mulu.w  #$400,d1
-	divu.w  d0,d1
+	move.w masterVolume(a5), d0
+	move.w d1, masterVolume(a5)
+	move.w systemVolume(a5), d1
+	lsr.w #4, d1
+	mulu.w #$400, d1
+	divu.w d0, d1
 
 @loc_277C:
 	; Update system volume
-	mulu.w  masterVolume(a5),d1
-	divu.w  #$400,d1
-	bclr    #2,volumeBitfield(a5)
-	bclr    #1,volumeBitfield(a5)
-	swap    d1
-	move.w  systemVolume(a5),d1
-	move.w  d1,word_5AC2(a5)
-	move.w  d1,word_5AC0(a5)
-	ror.w   #4,d1
-	lsl.l   #4,d1
-	swap    d1
-	move.w  d1,systemVolume(a5)
+	mulu.w masterVolume(a5), d1
+	divu.w #$400, d1
+	bclr #2, volumeBitfield(a5)
+	bclr #1, volumeBitfield(a5)
+	swap d1
+	move.w systemVolume(a5), d1
+	move.w d1, word_5AC2(a5)
+	move.w d1, word_5AC0(a5)
+	ror.w #4, d1
+	lsl.l #4, d1
+	swap d1
+	move.w d1, systemVolume(a5)
 
 	; Set volume update flag
-	bset    #7,volumeBitfield(a5)
+	bset #7, volumeBitfield(a5)
 	rts
 ; End of function _fdrset
 
@@ -68,7 +68,7 @@ _fdrset:                ; CODE XREF: sub_2946+22j initCdd+2Cp
 
 
 sub_27B0:
-	bset    #2,volumeBitfield(a5)
+	bset #2, volumeBitfield(a5)
 	rts
 ; End of function sub_27B0
 
@@ -77,9 +77,9 @@ sub_27B0:
 
 
 sub_27B8:
-	btst    #3,volumeBitfield(a5)
-	bne.s   @locret_27C6
-	bset    #2,volumeBitfield(a5)
+	btst #3, volumeBitfield(a5)
+	bne.s @locret_27C6
+	bset #2, volumeBitfield(a5)
 
 @locret_27C6:                ; CODE XREF: sub_27B8+6j
 	rts
@@ -90,9 +90,9 @@ sub_27B8:
 
 
 sub_27C8:
-	btst    #3,volumeBitfield(a5)
-	beq.s   @locret_27D6
-	bset    #2,volumeBitfield(a5)
+	btst #3, volumeBitfield(a5)
+	beq.s @locret_27D6
+	bset #2, volumeBitfield(a5)
 
 @locret_27D6:                ; CODE XREF: sub_27C8+6j
 	rts
@@ -103,11 +103,11 @@ sub_27C8:
 
 
 sub_27D8:               ; CODE XREF: BIOS:00001172p
-	move.b  #2,d0
-	bset    d0,systemVolume+1(a5)
-	bset    d0,word_5AC0+1(a5)
-	bset    d0,word_5AC2+1(a5)
-	bset    d0,word_5ABE+1(a5)
+	move.b #2, d0
+	bset d0, systemVolume+1(a5)
+	bset d0, word_5AC0+1(a5)
+	bset d0, word_5AC2+1(a5)
+	bset d0, word_5ABE+1(a5)
 	rts
 ; End of function sub_27D8
 
@@ -116,11 +116,11 @@ sub_27D8:               ; CODE XREF: BIOS:00001172p
 
 
 sub_27EE:               ; CODE XREF: BIOS:loc_1178p
-	move.b  #2,d0
-	bclr    d0,systemVolume+1(a5)
-	bclr    d0,word_5AC0+1(a5)
-	bclr    d0,word_5AC2+1(a5)
-	bclr    d0,word_5ABE+1(a5)
+	move.b #2, d0
+	bclr d0, systemVolume+1(a5)
+	bclr d0, word_5AC0+1(a5)
+	bclr d0, word_5AC2+1(a5)
+	bclr d0, word_5ABE+1(a5)
 	rts
 ; End of function sub_27EE
 
@@ -129,27 +129,27 @@ sub_27EE:               ; CODE XREF: BIOS:loc_1178p
 
 
 _fdrchg:                ; CODE XREF: sub_2946+26j
-	bclr    #1,volumeBitfield(a5)
-	move.w  d1,volumeSlope(a5)
-	swap    d1
-	mulu.w  masterVolume(a5),d1
-	divu.w  #$400,d1
-	swap    d1
-	move.w  systemVolume(a5),d1
-	ror.w   #4,d1
-	lsl.l   #4,d1
-	move.w  d1,d0
-	swap    d1
-	move.w  d1,word_5AC2(a5)
-	lsr.w   #4,d1
-	lsr.w   #4,d0
-	sub.w   d1,d0
-	bcc.s   @loc_2836
-	neg.w   volumeSlope(a5)
+	bclr #1, volumeBitfield(a5)
+	move.w d1, volumeSlope(a5)
+	swap d1
+	mulu.w masterVolume(a5), d1
+	divu.w #$400, d1
+	swap d1
+	move.w systemVolume(a5), d1
+	ror.w #4, d1
+	lsl.l #4, d1
+	move.w d1, d0
+	swap d1
+	move.w d1, word_5AC2(a5)
+	lsr.w #4, d1
+	lsr.w #4, d0
+	sub.w d1, d0
+	bcc.s @loc_2836
+	neg.w volumeSlope(a5)
 
 @loc_2836:               ; CODE XREF: _fdrchg+2Cj
-	move.w  d0,word_5AC4(a5)
-	bset    #1,volumeBitfield(a5)
+	move.w d0, word_5AC4(a5)
+	bset #1, volumeBitfield(a5)
 	rts
 ; End of function _fdrchg
 
@@ -158,10 +158,10 @@ _fdrchg:                ; CODE XREF: sub_2946+26j
 
 
 sub_2842:
-	move.l  masterVolume(a5),d0
-	lsr.w   #4,d0
-	move.b  volumeBitfield(a5),d1
-	lsr.b   #2,d1
+	move.l masterVolume(a5), d0
+	lsr.w #4, d0
+	move.b volumeBitfield(a5), d1
+	lsr.b #2, d1
 	rts
 ; End of function sub_2842
 
@@ -170,44 +170,44 @@ sub_2842:
 
 
 updateVolume:               ; CODE XREF: BIOS:00000626p
-	bset    #0,volumeBitfield(a5)
-	bne.s   @locret_28B2
-	btst    #7,volumeBitfield(a5)
-	beq.s   @loc_28AC
-	move.w  (GA_CDD_FADER).w,d0
-	btst    #$F,d0
-	bne.s   @loc_28AC
-	btst    #6,volumeBitfield(a5)
-	beq.s   @loc_287E
-	bclr    #2,volumeBitfield(a5)
-	beq.s   @loc_289A
-	bsr.s   sub_28B4
-	bra.s   @loc_289A
+	bset #0, volumeBitfield(a5)
+	bne.s @locret_28B2
+	btst #7, volumeBitfield(a5)
+	beq.s @loc_28AC
+	move.w (GA_CDD_FADER).w, d0
+	btst #$F, d0
+	bne.s @loc_28AC
+	btst #6, volumeBitfield(a5)
+	beq.s @loc_287E
+	bclr #2, volumeBitfield(a5)
+	beq.s @loc_289A
+	bsr.s sub_28B4
+	bra.s @loc_289A
 ; ---------------------------------------------------------------------------
 
 @loc_287E:               ; CODE XREF: updateVolume+20j
-	bclr    #2,volumeBitfield(a5)
-	beq.s   @loc_2888
-	bsr.s   sub_28CC
+	bclr #2, volumeBitfield(a5)
+	beq.s @loc_2888
+	bsr.s sub_28CC
 
 @loc_2888:               ; CODE XREF: updateVolume+34j
-	btst    #3,volumeBitfield(a5)
-	bne.s   @loc_289A
-	btst    #1,volumeBitfield(a5)
-	beq.s   @loc_289A
-	bsr.s   sub_28EA
+	btst #3, volumeBitfield(a5)
+	bne.s @loc_289A
+	btst #1, volumeBitfield(a5)
+	beq.s @loc_289A
+	bsr.s sub_28EA
 
 @loc_289A:               ; CODE XREF: updateVolume+28j
 					; updateVolume+2Cj ...
-	move.w  systemVolume(a5),d0
-	cmp.w   word_5ABC(a5),d0
-	beq.s   @loc_28AC
-	move.w  d0,word_5ABC(a5)
-	move.w  d0,(GA_CDD_FADER).w
+	move.w systemVolume(a5), d0
+	cmp.w word_5ABC(a5), d0
+	beq.s @loc_28AC
+	move.w d0, word_5ABC(a5)
+	move.w d0, (GA_CDD_FADER).w
 
 @loc_28AC:               ; CODE XREF: updateVolume+Ej
 					; updateVolume+18j ...
-	bclr    #0,volumeBitfield(a5)
+	bclr #0, volumeBitfield(a5)
 
 @locret_28B2:                ; CODE XREF: updateVolume+6j
 	rts
@@ -218,14 +218,14 @@ updateVolume:               ; CODE XREF: BIOS:00000626p
 
 
 sub_28B4:               ; CODE XREF: updateVolume+2Ap
-	bchg    #3,volumeBitfield(a5)
-	bne.s   @loc_28C4
-	move.w  word_5ABE(a5),word_5AC0(a5)
+	bchg #3, volumeBitfield(a5)
+	bne.s @loc_28C4
+	move.w word_5ABE(a5), word_5AC0(a5)
 	rts
 ; ---------------------------------------------------------------------------
 
 @loc_28C4:               ; CODE XREF: sub_28B4+6j
-	move.w  word_5AC0(a5),word_5ABE(a5)
+	move.w word_5AC0(a5), word_5ABE(a5)
 	rts
 ; End of function sub_28B4
 
@@ -234,15 +234,15 @@ sub_28B4:               ; CODE XREF: updateVolume+2Ap
 
 
 sub_28CC:               ; CODE XREF: updateVolume+36p
-	bchg    #3,volumeBitfield(a5)
-	bne.s   @loc_28E2
-	move.w  systemVolume(a5),word_5AC0(a5)
-	andi.w  #$F,systemVolume(a5)
+	bchg #3, volumeBitfield(a5)
+	bne.s @loc_28E2
+	move.w systemVolume(a5), word_5AC0(a5)
+	andi.w #$F, systemVolume(a5)
 	rts
 ; ---------------------------------------------------------------------------
 
 @loc_28E2:               ; CODE XREF: sub_28CC+6j
-	move.w  word_5AC0(a5),systemVolume(a5)
+	move.w word_5AC0(a5), systemVolume(a5)
 	rts
 ; End of function sub_28CC
 
@@ -251,32 +251,32 @@ sub_28CC:               ; CODE XREF: updateVolume+36p
 
 
 sub_28EA:               ; CODE XREF: updateVolume+48p
-	move.w  word_5AC4(a5),d0
-	bmi.s   @loc_2904
-	sub.w   volumeSlope(a5),d0
-	bcs.s   @loc_290A
+	move.w word_5AC4(a5), d0
+	bmi.s @loc_2904
+	sub.w volumeSlope(a5), d0
+	bcs.s @loc_290A
 
 @loc_28F6:               ; CODE XREF: sub_28EA+1Ej
-	moveq   #0,d1
-	move.w  word_5AC2(a5),d1
-	ror.l   #4,d1
-	add.w   d0,d1
-	rol.l   #4,d1
-	bra.s   @loc_291A
+	moveq #0, d1
+	move.w word_5AC2(a5), d1
+	ror.l #4, d1
+	add.w d0, d1
+	rol.l #4, d1
+	bra.s @loc_291A
 ; ---------------------------------------------------------------------------
 
 @loc_2904:               ; CODE XREF: sub_28EA+4j
-	sub.w   volumeSlope(a5),d0
-	bcs.s   @loc_28F6
+	sub.w volumeSlope(a5), d0
+	bcs.s @loc_28F6
 
 @loc_290A:               ; CODE XREF: sub_28EA+Aj
-	move.w  word_5AC2(a5),d1
-	moveq   #0,d0
-	clr.w   volumeSlope(a5)
-	bclr    #1,volumeBitfield(a5)
+	move.w word_5AC2(a5), d1
+	moveq #0, d0
+	clr.w volumeSlope(a5)
+	bclr #1, volumeBitfield(a5)
 
 @loc_291A:               ; CODE XREF: sub_28EA+18j
-	move.w  d1,systemVolume(a5)
-	move.w  d0,word_5AC4(a5)
+	move.w d1, systemVolume(a5)
+	move.w d0, word_5AC4(a5)
 	rts
 ; End of function sub_28EA
