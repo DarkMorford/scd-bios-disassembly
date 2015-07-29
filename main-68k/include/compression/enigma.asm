81c7ac09d176aaf32f70e340909cedd3da43658f
DarkMorford <DarkMorford@users.noreply.github.com>
2015-07-29 09:36:48 -0700

    Clean up decompression routines for readability.

--- main-68k/include/compression/enigma.asm
+++ main-68k/include/compression/enigma.asm
@@ -3,256 +3,294 @@
 ;   ======================================================================
 
 decompressEnigma:
+	movem.l d0-d7/a3-a6, -(sp)
 
-		movem.l	d0-d7/a3-a6,-(sp)
-		movea.w	d0,a3
-		move.b	(a1)+,d0
-		ext.w	d0
-		movea.w	d0,a5
-		move.b	(a1)+,d0
-		ext.w	d0
-		ext.l	d0
-		ror.l	#1,d0
-		ror.w	#1,d0
-		move.l	d0,d4
-		movea.w	(a1)+,a6
-		adda.w	a3,a6
-		movea.w	(a1)+,a4
-		adda.w	a3,a4
-		move.b	(a1)+,d5
-		asl.w	#8,d5
-		move.b	(a1)+,d5
-		moveq	#$10,d6
+	movea.w d0, a3
+
+	move.b  (a1)+, d0
+	ext.w   d0
+	movea.w d0, a5
+
+	move.b  (a1)+, d0
+	ext.w   d0
+	ext.l   d0
+	ror.l   #1, d0
+	ror.w   #1, d0
+	move.l  d0, d4
+
+	movea.w (a1)+, a6
+	adda.w  a3, a6
+
+	movea.w (a1)+, a4
+	adda.w  a3, a4
+
+	move.b  (a1)+, d5
+	asl.w   #8, d5
+	move.b  (a1)+, d5
+
+	moveq   #$10, d6
 
 loc_1AC8:
-		moveq	#7,d0
-		move.w	d6,d7
-		sub.w	d0,d7
-		move.w	d5,d1
-		lsr.w	d7,d1
-		andi.w	#$7F,d1
-		move.w	d1,d2
-		cmpi.w	#$40,d1
-		bcc.s	loc_1AE2
-		moveq	#6,d0
-		lsr.w	#1,d2
+	moveq  #7, d0
 
-loc_1AE2:
-		bsr.w	sub_1BEA
-		andi.w	#$F,d2
-		lsr.w	#4,d1
-		add.w	d1,d1
-		jmp	loc_1B3E(pc,d1.w)
+	move.w d6, d7
+	sub.w  d0, d7
+
+	move.w d5, d1
+	lsr.w  d7, d1
+	andi.w #$7F, d1
+
+	move.w d1, d2
+	cmpi.w #$40, d1
+	bcc.s  @loc_1AE2
+
+	moveq #6, d0
+	lsr.w #1, d2
+
+@loc_1AE2:
+	bsr.w sub_1BEA
+
+	andi.w #$F, d2
+
+	lsr.w #4, d1
+	add.w d1, d1
+	jmp loc_1B3E(pc, d1.w)
 ; End of function decompressEnigma
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1AF2:
+	move.w a6, (a2)+
+	addq.w #1, a6
+	dbf d2, sub_1AF2
 
-		move.w	a6,(a2)+
-		addq.w	#1,a6
-		dbf	d2,sub_1AF2
-		bra.s	loc_1AC8
+	bra.s  loc_1AC8
 ; End of function sub_1AF2
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1AFC:
-		move.w	a4,(a2)+
-		dbf	d2,sub_1AFC
-		bra.s	loc_1AC8
+	move.w a4, (a2)+
+	dbf d2, sub_1AFC
+
+	bra.s loc_1AC8
 ; End of function sub_1AFC
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1B04:
-		bsr.w	sub_1B66
+	bsr.w sub_1B66
 
-loc_1B08:
-		move.w	d1,(a2)+
-		dbf	d2,loc_1B08
-		bra.s	loc_1AC8
+	@loc_1B08:
+		move.w d1, (a2)+
+		dbf d2, @loc_1B08
+
+	bra.s loc_1AC8
 ; End of function sub_1B04
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1B10:
-		bsr.w	sub_1B66
+	bsr.w sub_1B66
 
-loc_1B14:
-		move.w	d1,(a2)+
-		addq.w	#1,d1
-		dbf	d2,loc_1B14
-		bra.s	loc_1AC8
+	@loc_1B14:
+		move.w d1, (a2)+
+		addq.w #1, d1
+		dbf d2, @loc_1B14
+
+	bra.s loc_1AC8
 ; End of function sub_1B10
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1B1E:
-		bsr.w	sub_1B66
+	bsr.w sub_1B66
 
-loc_1B22:
-		move.w	d1,(a2)+
-		subq.w	#1,d1
-		dbf	d2,loc_1B22
-		bra.s	loc_1AC8
+	@loc_1B22:
+		move.w d1, (a2)+
+		subq.w #1, d1
+		dbf d2, @loc_1B22
+
+	bra.s loc_1AC8
 ; End of function sub_1B1E
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1B2C:
-		cmpi.w	#$F,d2
-		beq.s	loc_1B4E
+	cmpi.w #$F, d2
+	beq.s  loc_1B4E
 
-loc_1B32:
-		bsr.w	sub_1B66
-		move.w	d1,(a2)+
-		dbf	d2,loc_1B32
-		bra.s	loc_1AC8
+	@loc_1B32:
+		bsr.w  sub_1B66
+		move.w d1, (a2)+
+		dbf d2, @loc_1B32
+
+	bra.s loc_1AC8
 ; ---------------------------------------------------------------------------
 
 loc_1B3E:
-		bra.s	sub_1AF2
+	bra.s   sub_1AF2
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1AF2
+	bra.s   sub_1AF2
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1AFC
+	bra.s   sub_1AFC
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1AFC
+	bra.s   sub_1AFC
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1B04
+	bra.s   sub_1B04
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1B10
+	bra.s   sub_1B10
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1B1E
+	bra.s   sub_1B1E
 ; ---------------------------------------------------------------------------
-		bra.s	sub_1B2C
+	bra.s   sub_1B2C
 ; ---------------------------------------------------------------------------
 
 loc_1B4E:
-		subq.w	#1,a1
-		cmpi.w	#$10,d6
-		bne.s	loc_1B58
-		subq.w	#1,a1
+	subq.w #1, a1
 
-loc_1B58:
-		move.w	a1,d0
-		lsr.w	#1,d0
-		bcc.s	loc_1B60
-		addq.w	#1,a1
+	cmpi.w #$10, d6
+	bne.s  @loc_1B58
 
-loc_1B60:
-		movem.l	(sp)+,d0-d7/a3-a6
-		rts
+	subq.w #1, a1
+
+@loc_1B58:
+	move.w a1, d0
+	lsr.w  #1, d0
+	bcc.s  @loc_1B60
+
+	addq.w #1, a1
+
+@loc_1B60:
+	movem.l (sp)+, d0-d7/a3-a6
+	rts
 ; End of function sub_1B2C
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1B66:
-		move.w	a3,d3
-		swap	d4
-		bpl.s	loc_1B76
-		subq.w	#1,d6
-		btst	d6,d5
-		beq.s	loc_1B76
-		ori.w	#$1000,d3
+	move.w a3, d3
+	swap   d4
+	bpl.s  @loc_1B76
 
-loc_1B76:
-		swap	d4
-		bpl.s	loc_1B84
-		subq.w	#1,d6
-		btst	d6,d5
-		beq.s	loc_1B84
-		ori.w	#$800,d3
+	subq.w #1, d6
+	btst   d6, d5
+	beq.s  @loc_1B76
 
-loc_1B84:
-		move.w	d5,d1
-		move.w	d6,d7
-		sub.w	a5,d7
-		bcc.s	loc_1BB4
-		move.w	d7,d6
-		addi.w	#$10,d6
-		neg.w	d7
-		lsl.w	d7,d1
-		move.b	(a1),d5
-		rol.b	d7,d5
-		add.w	d7,d7
-		and.w	loc_1BC8(pc,d7.w),d5
-		add.w	d5,d1
+	ori.w  #$1000, d3
 
-loc_1BA2:
-		move.w	a5,d0
-		add.w	d0,d0
-		and.w	loc_1BC8(pc,d0.w),d1
-		add.w	d3,d1
-		move.b	(a1)+,d5
-		lsl.w	#8,d5
-		move.b	(a1)+,d5
-		rts
+@loc_1B76:
+	swap   d4
+	bpl.s  @loc_1B84
+
+	subq.w #1, d6
+	btst   d6, d5
+	beq.s  @loc_1B84
+
+	ori.w  #$800, d3
+
+@loc_1B84:
+	move.w d5, d1
+
+	move.w d6, d7
+	sub.w  a5, d7
+	bcc.s  @loc_1BB4
+
+	move.w d7, d6
+	addi.w #$10, d6
+
+	neg.w  d7
+	lsl.w  d7, d1
+
+	move.b (a1), d5
+	rol.b  d7, d5
+
+	add.w  d7, d7
+	and.w  @loc_1BC8(pc, d7.w), d5
+
+	add.w  d5, d1
+
+@loc_1BA2:
+	move.w a5, d0
+	add.w  d0, d0
+	and.w  @loc_1BC8(pc, d0.w), d1
+	add.w  d3, d1
+
+	move.b (a1)+, d5
+	lsl.w  #8, d5
+	move.b (a1)+, d5
+
+	rts
 ; ---------------------------------------------------------------------------
 
-loc_1BB4:
-		beq.s	loc_1BC6
-		lsr.w	d7,d1
-		move.w	a5,d0
-		add.w	d0,d0
-		and.w	loc_1BC8(pc,d0.w),d1
-		add.w	d3,d1
-		move.w	a5,d0
-		bra.s	sub_1BEA
+@loc_1BB4:
+	beq.s  @loc_1BC6
+
+	lsr.w  d7, d1
+
+	move.w a5, d0
+	add.w  d0, d0
+
+	and.w  @loc_1BC8(pc, d0.w), d1
+	add.w  d3, d1
+
+	move.w a5, d0
+
+	bra.s  sub_1BEA
 ; ---------------------------------------------------------------------------
 
-loc_1BC6:
-		moveq	#$10,d6
+@loc_1BC6:
+	moveq #$10, d6
 
-loc_1BC8:
-		bra.s	loc_1BA2
+@loc_1BC8:
+	bra.s @loc_1BA2
 ; End of function sub_1B66
 
 ; ---------------------------------------------------------------------------
-		dc.w 1
-		dc.w 3
-		dc.w 7
-		dc.w $F
-		dc.w $1F
-		dc.w $3F
-		dc.w $7F
-		dc.w $FF
-		dc.w $1FF
-		dc.w $3FF
-		dc.w $7FF
-		dc.w $FFF
-		dc.w $1FFF
-		dc.w $3FFF
-		dc.w $7FFF
-		dc.w $FFFF
+word_1BCA:
+	dc.w     1
+	dc.w     3
+	dc.w     7
+	dc.w    $F
+	dc.w   $1F
+	dc.w   $3F
+	dc.w   $7F
+	dc.w   $FF
+	dc.w  $1FF
+	dc.w  $3FF
+	dc.w  $7FF
+	dc.w  $FFF
+	dc.w $1FFF
+	dc.w $3FFF
+	dc.w $7FFF
+	dc.w $FFFF
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_1BEA:
+	sub.w  d0, d6
+	cmpi.w #9, d6
+	bcc.s  locret_1BF8
 
-		sub.w	d0,d6
-		cmpi.w	#9,d6
-		bcc.s	locret_1BF8
-		addq.w	#8,d6
-		asl.w	#8,d5
-		move.b	(a1)+,d5
+	addq.w #8, d6
+	asl.w  #8, d5
+	move.b (a1)+, d5
 
 locret_1BF8:
-		rts
+	rts
 ; End of function sub_1BEA
--- main-68k/include/compression/nemesis.asm
+++ main-68k/include/compression/nemesis.asm
@@ -3,214 +3,240 @@
 ;   ======================================================================
 
 decompressNemesis:
+	movem.l d0-d7/a1-a5, -(sp)
+	lea sub_F0A(pc),  a3
+	lea (VDP_DATA).l, a2
 
-		movem.l	d0-d7/a1-a5,-(sp)
-		lea	sub_F0A(pc),a3
-		lea	(VDP_DATA).l,a2
-		bra.s	loc_E9C
+	bra.s loc_E9C
 ; ---------------------------------------------------------------------------
 
 decompressNemesisToRam:
-		movem.l	d0-d7/a1-a5,-(sp)
-		lea	loc_F20(pc),a3
+	movem.l d0-d7/a1-a5, -(sp)
+	lea loc_F20(pc), a3
 
 loc_E9C:
-		lea	(nemesisCodeTable).w,a4
-		move.w	(a1)+,d2
-		add.w	d2,d2
-		bcc.s	loc_EAA
-		adda.w	#$A,a3
+	lea (nemesisCodeTable).w, a4
+
+	move.w (a1)+, d2
+	add.w  d2, d2
+	bcc.s  loc_EAA
+
+	adda.w #$A, a3
 
 loc_EAA:
-		lsl.w	#2,d2
-		movea.w	d2,a5
-		moveq	#8,d3
-		moveq	#0,d2
-		moveq	#0,d4
-		bsr.w	sub_F3A
-		move.b	(a1)+,d5
-		asl.w	#8,d5
-		move.b	(a1)+,d5
-		moveq	#$10,d6
+	lsl.w   #2, d2
+	movea.w d2, a5
+	moveq   #8, d3
+	moveq   #0, d2
+	moveq   #0, d4
+	bsr.w   sub_F3A
+
+	move.b (a1)+, d5
+	asl.w  #8, d5
+	move.b (a1)+, d5
+	moveq  #$10, d6
 
 loc_EC0:
-		moveq	#8,d0
-		bsr.w	sub_F9C
-		cmpi.w	#$FC,d1
-		bcc.s	loc_EFC
-		add.w	d1,d1
-		move.b	(a4,d1.w),d0
-		ext.w	d0
-		bsr.w	sub_FB0
-		move.b	1(a4,d1.w),d1
+	moveq  #8, d0
+	bsr.w  sub_F9C
+
+	cmpi.w #$FC, d1
+	bcc.s  loc_EFC
+
+	add.w  d1, d1
+	move.b (a4, d1.w), d0
+	ext.w  d0
+	bsr.w  sub_FB0
+
+	move.b 1(a4, d1.w), d1
 
 loc_EDC:
-		move.w	d1,d0
-		andi.w	#$F,d1
-		andi.w	#$F0,d0
-		lsr.w	#4,d0
+	move.w d1, d0
+	andi.w #$F, d1
+	andi.w #$F0, d0
+	lsr.w  #4, d0
 
 loc_EE8:
-		lsl.l	#4,d4
-		or.b	d1,d4
-		subq.w	#1,d3
-		bne.s	loc_EF6
-		jmp	(a3)
+	lsl.l  #4, d4
+	or.b   d1, d4
+	subq.w #1, d3
+	bne.s  loc_EF6
+	jmp (a3)
 ; ---------------------------------------------------------------------------
 
 loc_EF2:
-		moveq	#0,d4
-		moveq	#8,d3
+	moveq #0, d4
+	moveq #8, d3
 
 loc_EF6:
-		dbf	d0,loc_EE8
-		bra.s	loc_EC0
+	dbf d0, loc_EE8
+
+	bra.s loc_EC0
 ; ---------------------------------------------------------------------------
 
 loc_EFC:
-		moveq	#6,d0
-		bsr.w	sub_FB0
-		moveq	#7,d0
-		bsr.w	sub_FAC
-		bra.s	loc_EDC
+	moveq #6, d0
+	bsr.w sub_FB0
+
+	moveq #7, d0
+	bsr.w sub_FAC
+
+	bra.s loc_EDC
 ; End of function decompressNemesis
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_F0A:
-		move.l	d4,(a2)
-		subq.w	#1,a5
-		move.w	a5,d4
-		bne.s	loc_EF2
-		bra.s	loc_F34
+	move.l d4, (a2)
+	subq.w #1, a5
+
+	move.w a5, d4
+	bne.s  loc_EF2
+	bra.s  loc_F34
 ; ---------------------------------------------------------------------------
-		eor.l	d4,d2
-		move.l	d2,(a2)
-		subq.w	#1,a5
-		move.w	a5,d4
-		bne.s	loc_EF2
-		bra.s	loc_F34
+loc_F14:
+	eor.l  d4, d2
+	move.l d2, (a2)
+	subq.w #1, a5
+
+	move.w a5, d4
+	bne.s  loc_EF2
+	bra.s  loc_F34
 ; ---------------------------------------------------------------------------
 
 loc_F20:
-		move.l	d4,(a2)+
-		subq.w	#1,a5
-		move.w	a5,d4
-		bne.s	loc_EF2
-		bra.s	loc_F34
+	move.l d4, (a2)+
+	subq.w #1, a5
+
+	move.w a5, d4
+	bne.s  loc_EF2
+	bra.s  loc_F34
 ; ---------------------------------------------------------------------------
-		eor.l	d4,d2
-		move.l	d2,(a2)+
-		subq.w	#1,a5
-		move.w	a5,d4
-		bne.s	loc_EF2
+loc_F2A:
+	eor.l  d4, d2
+	move.l d2, (a2)+
+	subq.w #1, a5
+
+	move.w a5, d4
+	bne.s  loc_EF2
 
 loc_F34:
-		movem.l	(sp)+,d0-d7/a1-a5
-		rts
+	movem.l (sp)+, d0-d7/a1-a5
+	rts
 ; End of function sub_F0A
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_F3A:
-		move.b	(a1)+,d0
+	move.b (a1)+, d0
 
 loc_F3C:
-		cmpi.b	#$FF,d0
-		bne.s	loc_F44
-		rts
+	cmpi.b #$FF, d0
+	bne.s  @loc_F44
+	rts
 ; ---------------------------------------------------------------------------
 
-loc_F44:
-		move.w	d0,d7
+@loc_F44:
+	move.w d0, d7
 
 loc_F46:
+	move.b (a1)+, d0
+	bmi.s  loc_F3C
 
-		move.b	(a1)+,d0
-		bmi.s	loc_F3C
-		move.b	d0,d1
-		andi.w	#$F,d7
-		andi.w	#$70,d1
-		or.w	d1,d7
-		andi.w	#$F,d0
-		move.b	d0,d1
-		lsl.w	#8,d1
-		or.w	d1,d7
-		moveq	#8,d1
-		sub.w	d0,d1
-		bne.s	loc_F70
-		move.b	(a1)+,d0
-		add.w	d0,d0
-		move.w	d7,(a4,d0.w)
-		bra.s	loc_F46
+	move.b d0, d1
+	andi.w #$F, d7
+	andi.w #$70, d1
+	or.w   d1, d7
+
+	andi.w #$F, d0
+
+	move.b d0, d1
+	lsl.w  #8, d1
+	or.w   d1, d7
+	moveq  #8, d1
+
+	sub.w  d0, d1
+	bne.s  @loc_F70
+
+	move.b (a1)+, d0
+	add.w  d0, d0
+	move.w d7, (a4, d0.w)
+	bra.s  loc_F46
 ; ---------------------------------------------------------------------------
 
-loc_F70:
-		move.b	(a1)+,d0
-		lsl.w	d1,d0
-		add.w	d0,d0
-		moveq	#1,d5
-		lsl.w	d1,d5
-		subq.w	#1,d5
-		lea	(a4,d0.w),a4
+@loc_F70:
+	move.b (a1)+, d0
+	lsl.w  d1, d0
+	add.w  d0, d0
 
-loc_F80:
-		move.w	d7,(a4)+
-		dbf	d5,loc_F80
-		lea	(nemesisCodeTable).w,a4
+	moveq  #1, d5
+	lsl.w  d1, d5
+	subq.w #1, d5
+
+	lea (a4, d0.w), a4
+
+	@loc_F80:
+		move.w d7, (a4)+
+		dbf d5, @loc_F80
+
+	lea (nemesisCodeTable).w, a4
 
 loc_F8A:
-		bra.s	loc_F46
+	bra.s loc_F46
 ; End of function sub_F3A
 
 ; ---------------------------------------------------------------------------
-		dc.w 1
-		dc.w 3
-		dc.w 7
-		dc.w $F
-		dc.w $1F
-		dc.w $3F
-		dc.w $7F
-		dc.w $FF
+word_F8C:
+	dc.w   1
+	dc.w   3
+	dc.w   7
+	dc.w  $F
+	dc.w $1F
+	dc.w $3F
+	dc.w $7F
+	dc.w $FF
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_F9C:
-		move.w	d6,d7
-		sub.w	d0,d7
-		move.w	d5,d1
-		lsr.w	d7,d1
-		add.w	d0,d0
-		and.w	loc_F8A(pc,d0.w),d1
-		rts
+	move.w d6, d7
+	sub.w  d0, d7
+
+	move.w d5, d1
+	lsr.w  d7, d1
+
+	add.w d0, d0
+	and.w loc_F8A(pc, d0.w), d1
+	rts
 ; End of function sub_F9C
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_FAC:
-		bsr.s	sub_F9C
-		lsr.w	#1,d0
+	bsr.s sub_F9C
+	lsr.w #1, d0
 ; End of function sub_FAC
 
 
-; =============== S U B	R O U T	I N E =======================================
+; =============== S U B R O U T I N E =======================================
 
 
 sub_FB0:
-		sub.w	d0,d6
-		cmpi.w	#9,d6
-		bcc.s	locret_FBE
-		addq.w	#8,d6
-		asl.w	#8,d5
-		move.b	(a1)+,d5
+	sub.w  d0, d6
+	cmpi.w #9, d6
+	bcc.s  @locret_FBE
 
-locret_FBE:
-		rts
+	addq.w #8, d6
+	asl.w  #8, d5
+	move.b (a1)+, d5
+
+@locret_FBE:
+	rts
 ; End of function sub_FB0
